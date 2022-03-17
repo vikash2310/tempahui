783683092bfc99482744e3a25b4217ca65f7d054
Arime <arime@pop-os.localdomain>
2022-03-17 17:53:30 +0530

    added

---
+++ cool.py
@@ -0,0 +1,14 @@
+
+
+
+
+instructions = ['big table', 
+                'small table',
+                'big chair',
+                'small chair']
+
+
+
+
+instructions.append('hey there')
+print(instructions)
---
+++ web.py
@@ -0,0 +1,10 @@
+#webscraper tutorial
+
+print("Welcome to the webscraper tutorial")
+
+
+something_true = True
+while something_true:
+    print("This is a while loop")
+    something_true = False
+    print("what th eheck is this")
